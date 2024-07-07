1569ddf53c24022bdc6d7b951f360d9d91516335
unknown <robpoquin@gmail.com>
2024-07-07 17:44:01 +0200

    Stockage des taches dans le navigateur (JSON storage)

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,7 +5,7 @@ import TodoItems from './TodoItems'
 const Todo = () => {
 
   const inputRef = useRef();
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(localStorage.getItem("todos")? JSON.parse(localStorage.getItem("todos")): []);
 
   const addTask = () => {
     const inputText = inputRef.current.value.trim();
@@ -41,7 +41,7 @@ const Todo = () => {
   }
 
   useEffect(()=>{
-    //console.log(todoList);
+    localStorage.setItem("todos", JSON.stringify(todoList))
   },[todoList])
 
   return (
