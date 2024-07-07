901e4987f8843cd828944268a410310a894f3ca2
unknown <robpoquin@gmail.com>
2024-07-07 13:22:04 +0200

    Suppression  de tache de la liste

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -23,6 +23,12 @@ const Todo = () => {
     inputRef.current.value = "";
   }
 
+  const deleteTask = (id) => {
+    setTodoList((prvTodo)=> {
+      return prvTodo.filter((todo)=> todo.id !== id)
+    })
+  }
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
 
@@ -39,7 +45,7 @@ const Todo = () => {
 {/*---------- To Do List --------- */}
         <div>
           {todoList.map((item, index)=>{
-            return <TodoItems key={index} text={item.text}/>
+            return <TodoItems key={index} text={item.text} id={item.id} isCompleted={item.isCompleted} deleteTask={deleteTask}/>
           })}
         </div>
 
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -3,7 +3,7 @@ import tick from '../assets/tick.png'
 import not_tick from '../assets/not_tick.png'
 import delete_icon from '../assets/delete.png'
 
-const TodoItems = ({text}) => {
+const TodoItems = ({text, id, isCompleted, deleteTask}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
         <div className='flex flex-1 items-center cursor-pointer'>
@@ -13,7 +13,7 @@ const TodoItems = ({text}) => {
             </p>
         </div>
 
-        <img src={delete_icon} alt="" className='w-3.5 cursor-pointer'/>
+        <img onClick={() => {deleteTask(id)}} src={delete_icon} alt="" className='w-3.5 cursor-pointer'/>
     </div>
   )
 }
